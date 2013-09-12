6a167b2c2bad692a47467266e134d1c1aa2f121f
Thiago Augusto <thiago.eti.br@gmail.com>
2013-09-11 23:56:36 -0300

    Construção do footer

--- wp-content/themes/twentytwelve/footer.php
+++ wp-content/themes/twentytwelve/footer.php
@@ -12,10 +12,22 @@
 ?>
 	</div><!-- #main .wrapper -->
 	<footer id="colophon" role="contentinfo">
-		<div class="site-info">
-			<?php do_action( 'twentytwelve_credits' ); ?>
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentytwelve' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentytwelve' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentytwelve' ), 'WordPress' ); ?></a>
+		<div class="site-info">			
+			
+		</nav><!-- #site-navigation -->
 		</div><!-- .site-info -->
+
+		<div class="block-left">
+			<p>SIA Trecho 01 lotes 1050/1120 Guará, DF, Brasil - CEP: 71200-010</p>
+			<p></p>
+		</div>
+
+		<div class="block-right">
+			<nav role="navigation">
+			<h3 class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></h3>
+			<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu-footer' ) ); ?>
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
